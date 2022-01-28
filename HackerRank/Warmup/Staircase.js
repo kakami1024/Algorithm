48ea47ab11a64c31697542cd57bc16fa989df90e
kakami1024 <kakami1024@gmail.com>
2022-01-28 09:14:04 +0900

    0128"hackerrank 추가"

---
+++ HackerRank/Warmup/Staircase.js
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+
+function staircase(n) {
+  for(let i = 1; i <= n; i++){
+      console.log(' '.repeat(n - i) + '#'.repeat(i))
+  }
+}
